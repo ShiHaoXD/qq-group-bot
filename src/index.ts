4533495df6f2768bef0647c781dc6ba6689923a3
Cansiny0320 <1136843910@qq.com>
2021-06-04 16:29:05 +0800

    fix: 清除cache 获取新的memberlist

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ dotenv.config()
 
 const groupID: number = parseInt(process.env.GROUP_ID!)
 
-const messages: GroupMessageEventData[] = []
+// const messages: GroupMessageEventData[] = []
 const recentMessages: GroupMessageEventData[] = []
 let groupMemberList: ReadonlyMap<number, MemberInfo> | null
 const uin = parseInt(process.env.ACCOUNT!) // your account
@@ -18,7 +18,7 @@ const client = createClient(uin)
 const helper = new Helper(client, groupID)
 
 async function GroupMemberCardChanged() {
-  const newGroupMemberList = (await client.getGroupMemberList(groupID)).data
+  const newGroupMemberList = (await client.getGroupMemberList(groupID, true)).data
   let change = false
   if (newGroupMemberList) {
     newGroupMemberList.forEach((value, key) => {
@@ -45,6 +45,7 @@ client.on("system.online", async () => {
   // client.sendGroupMsg(groupID, "群机器人已开启")
   groupMemberList = (await client.getGroupMemberList(groupID)).data
   if (groupMemberList) {
+    console.log("群成员获取成功")
     setTimeout(GroupMemberCardChanged, 3000)
   } else {
     console.log("群成员获取失败")
@@ -66,7 +67,7 @@ client.on("message.group", async data => {
         return
       }
     }
-    messages.push(data)
+    // messages.push(data)
     recentMessages.push(data)
 
     if (recentMessages.length > canrepeatTimes) {
@@ -77,25 +78,25 @@ client.on("message.group", async data => {
   }
 })
 
-client.on("notice.group.recall", async data => {
-  let message: GroupMessageEventData | undefined
-  if (data.group_id === groupID) {
-    messages.some(item => {
-      if (item.message_id === data.message_id) {
-        message = item
-        return true
-      }
-    })
-    if (message) {
-      const operator = await client.getGroupMemberInfo(data.group_id, data.operator_id)
-      if (operator.status === "ok") {
-        helper.sendMsg(
-          `${operator.data.card}(${operator.data.user_id})撤回了${message.sender.card}(${message.sender.user_id})的一条消息：${message.raw_message}`,
-        )
-      }
-    }
-  }
-})
+// client.on("notice.group.recall", async data => {
+//   let message: GroupMessageEventData | undefined
+//   if (data.group_id === groupID) {
+//     messages.some(item => {
+//       if (item.message_id === data.message_id) {
+//         message = item
+//         return true
+//       }
+//     })
+//     if (message) {
+//       const operator = await client.getGroupMemberInfo(data.group_id, data.operator_id)
+//       if (operator.status === "ok") {
+//         helper.sendMsg(
+//           `${operator.data.card}(${operator.data.user_id})撤回了${message.sender.card}(${message.sender.user_id})的一条消息：${message.raw_message}`,
+//         )
+//       }
+//     }
+//   }
+// })
 
 //监听滑动验证码事件并输入ticket
 client.on("system.login.slider", function () {

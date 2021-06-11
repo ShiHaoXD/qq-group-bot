c9a7e63829c3fdf1a0239ce320c03670bfa45975
Cansiny0320 <1136843910@qq.com>
2021-06-12 01:58:18 +0800

    feat: 封装axios方法

---
+++ src/shared/http.ts
@@ -0,0 +1,29 @@
+import axios from 'axios';
+
+export const createAxiosInstance = (baseURL: string, headers: any) => {
+  const instance = axios.create({
+    baseURL,
+    headers,
+    timeout: 6000,
+  });
+
+  async function get(url: string, params: any = {}) {
+    try {
+      return instance.get(url, {params});
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
+  async function post(url: string, data: any = {}, config: any = {}) {
+    try {
+      return instance.post(url, data, config);
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+  return {
+    get,
+    post,
+  };
+};

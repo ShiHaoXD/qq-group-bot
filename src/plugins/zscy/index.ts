18c8d9ed9b401c0093f1bc0cea20aa0070eff7ca
Cansiny0320 <1136843910@qq.com>
2021-06-12 01:08:47 +0800

    feat: 签到详细状态

--- src/plugins/zscy/index.ts
+++ src/plugins/zscy/index.ts
@@ -7,7 +7,8 @@ import qs from 'qs';
 
 const checkInAPI =
   'https://be-prod.redrock.team/magipoke-intergral/QA/Integral/checkIn';
-
+const cheackInStatusAPI =
+  'https://be-prod.redrock.team/magipoke-intergral/QA/User/getScoreStatus';
 const options = {
   headers: {
     'Content-Type': 'application/x-www-form-urlencoded',
@@ -18,11 +19,25 @@ function checkIn() {
   zscyAccounts.forEach(async account => {
     const {data} = await axios.post(checkInAPI, qs.stringify(account), options);
     switch (data.status) {
-      case 200:
-        helper.sendMsg(`${account.stunum}签到成功`);
+      case 200: {
+        const {data: statusData} = await axios.post(
+          cheackInStatusAPI,
+          qs.stringify(account),
+          options
+        );
+        const {
+          data: {check_in_days, rank, percent},
+          status,
+        } = statusData;
+        if (status === 200) {
+          helper.sendMsg(
+            `${account.stunum} 签到成功\n今日排名第 ${rank}名\n已连续签到${check_in_days}天，超越了${percent}的人`
+          );
+        }
         break;
+      }
       case 403:
-        helper.sendMsg(`${account.stunum}签到失败，今日已签到`);
+        helper.sendMsg(`${account.stunum} 签到失败，今日已签到`);
         break;
     }
   });

8dd306afa8f7c853d45943a2eef050e9640651fc
Cansiny0320 <1136843910@qq.com>
2021-06-13 13:01:59 +0800

    fix: 错误导入

--- src/plugins/zscy/api.ts
+++ src/plugins/zscy/api.ts
@@ -1,5 +1,5 @@
 import {createAxiosInstance} from '../../shared/http';
-import qs from 'qs';
+import {stringify} from 'qs';
 const baseURL = 'https://be-prod.redrock.team/magipoke-intergral/QA';
 const headers = {
   'Content-Type': 'application/x-www-form-urlencoded',
@@ -8,7 +8,7 @@ const headers = {
 const {post} = createAxiosInstance(baseURL, headers);
 
 export const checkIn = (data: any) =>
-  post('/Integral/checkIn', qs.stringify(data));
+  post('/Integral/checkIn', stringify(data));
 
 export const getCheckInStatus = (data: any) =>
-  post('/User/getScoreStatus', qs.stringify(data));
+  post('/User/getScoreStatus', stringify(data));

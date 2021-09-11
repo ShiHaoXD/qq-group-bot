d1b309feeae4a861a003b13dd3a829b7a24e85b9
Cansiny0320 <1136843910@qq.com>
2021-09-12 00:17:49 +0800

    fix: 掌邮签到

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -1,22 +1,22 @@
 import news from './news';
-// import weCqupt from './weCqupt';
-// import whatToEat from './whatToEat';
-// import banForRepeat from './banForRepeat';
-// import replyKeyword from './replyKeyword';
-// import recallKeyword from './recallKeyword';
-// import zscy from './zscy';
-// import jwzx from './jwzx';
-// import greet from './greet';
-// import weather from './weather';
+import weCqupt from './weCqupt';
+import whatToEat from './whatToEat';
+import banForRepeat from './banForRepeat';
+import replyKeyword from './replyKeyword';
+import recallKeyword from './recallKeyword';
+import zscy from './zscy';
+import jwzx from './jwzx';
+import greet from './greet';
+import weather from './weather';
 export const plugins = [
   news,
-  // weCqupt,
-  // whatToEat,
-  // banForRepeat,
-  // replyKeyword,
-  // recallKeyword,
-  // zscy,
-  // jwzx,
-  // greet,
-  // weather,
+  weCqupt,
+  whatToEat,
+  banForRepeat,
+  replyKeyword,
+  recallKeyword,
+  zscy,
+  jwzx,
+  greet,
+  weather,
 ];
--- src/plugins/zscy/index.ts
+++ src/plugins/zscy/index.ts
@@ -1,6 +1,6 @@
 import {Plugin} from '../../shared/types';
 import {scheduleJob} from 'node-schedule';
-import {zscyAccounts} from './config.example';
+import {zscyAccounts} from './config.private';
 import {helper} from '../../bot';
 import {checkIn as checkInAPI, getCheckInStatus} from './api';
 
@@ -8,17 +8,14 @@ function checkIn() {
   zscyAccounts.forEach(async account => {
     const {data} = await checkInAPI(account);
     switch (data.status) {
-      case 200: {
+      case 10000: {
         const {data: statusData} = await getCheckInStatus(account);
         const {
           data: {check_in_days, rank, percent},
-          status,
         } = statusData;
-        if (status === 200) {
-          helper.sendMsg(
-            `${account.stunum} 签到成功\n今日第${rank}位签到\n已连续签到${check_in_days}天，超过${percent}的邮子`
-          );
-        }
+        helper.sendMsg(
+          `${account.stunum} 签到成功\n今日第${rank}位签到\n已连续签到${check_in_days}天，超过${percent}的邮子`
+        );
         break;
       }
       case 403:

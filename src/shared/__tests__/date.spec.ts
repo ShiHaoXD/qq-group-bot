a8c66170901f12487b12d9818b235ac9f9e7d148
Cansiny0320 <1136843910@qq.com>
2022-02-27 11:29:38 +0800

    test(date): add test name

--- src/shared/__tests__/date.spec.ts
+++ src/shared/__tests__/date.spec.ts
@@ -15,7 +15,7 @@ test('getNowTimestamp', () => {
   expect(getNowTimestamp()).eq(Math.floor(+new Date() / 1000));
 });
 
-test('', () => {
+test('getNowTimeString', () => {
   // example 2022-02-27 00:39:00
   const timeStringReg =
     /^\d{1,4}(-)(1[0-2]|0?[1-9])\1(0?[1-9]|[1-2]\d|30|31) (?:[01]\d|2[0-3]):[0-5]\d:[0-5]\d$/;

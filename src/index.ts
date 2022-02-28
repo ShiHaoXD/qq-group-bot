962be320406a201a6906e981c16f18f4cf557000
Cansiny0320 <1136843910@qq.com>
2022-02-28 18:57:30 +0800

    chore: use plugin

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,15 @@ import {createBot} from './bot';
 
 import {config} from 'dotenv';
 import {Greet} from './plugins/greet';
+import {ReplyKeyword} from './plugins/replyKeyword';
+import {BanForRepeat} from './plugins/banForRepeat';
+import {RecallKeyword} from './plugins/recallKeyword';
+import {WeCqupt} from './plugins/weCqupt';
+import {News} from './plugins/news';
+import {Jwzx} from './plugins/jwzx';
+import {WeatherForecast} from './plugins/weather';
+import {WhatToEat} from './plugins/whatToEat';
+import {HandleCqupt} from './plugins/zscy';
 
 config();
 
@@ -11,3 +20,12 @@ export const groupID = parseInt(process.env.GROUP_ID!);
 const bot = createBot(account, password, groupID);
 
 bot.use(Greet);
+bot.use(ReplyKeyword);
+bot.use(BanForRepeat);
+bot.use(RecallKeyword);
+bot.use(WeCqupt);
+bot.use(News);
+bot.use(Jwzx);
+bot.use(WeatherForecast);
+bot.use(WhatToEat);
+bot.use(HandleCqupt);

17516685dd7f31137b2efd8af7ae7d58c07cccbd
Cansiny0320 <1136843910@qq.com>
2022-03-06 03:00:01 +0800

    feat(weather): 增加天气 api 的类型和重试

--- src/plugins/weather/api.ts
+++ src/plugins/weather/api.ts
@@ -1,6 +1,26 @@
-import axios from 'axios';
+import {createAxiosInstance} from '../../shared/http';
+
+const {get} = createAxiosInstance(
+  'https://devapi.qweather.com/v7/weather',
+  {},
+  {retry: true}
+);
+
+export interface GetWeatherRes {
+  code: string;
+  updateTime: string;
+  hourly: HourlyData[];
+}
+
+export interface HourlyData {
+  fxTime: string; // 预报时间
+  temp: string; // 温度
+  text: string; // 天气状况
+}
+
+// 完整接口文档：https://dev.qweather.com/docs/api/weather/weather-hourly-forecast/
 
 export const getWeather = () =>
-  axios.get(
-    'https://devapi.qweather.com/v7/weather/24h?key=b6604b9d2e8d41ba90a5c2c6ea35a3b7&location=106.36,29.32'
+  get<GetWeatherRes>(
+    '/24h?key=b6604b9d2e8d41ba90a5c2c6ea35a3b7&location=106.36,29.32'
   );

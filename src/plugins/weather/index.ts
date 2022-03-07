10ddfb46c7da4b0971643a518b5858109d355c0c
Cansiny0320 <1136843910@qq.com>
2022-03-07 15:20:14 +0800

    chore: 捕获错误

--- src/plugins/weather/index.ts
+++ src/plugins/weather/index.ts
@@ -1,17 +1,22 @@
 import {scheduleJob} from 'node-schedule';
 import type Helper from '../../Helper';
+import {logger} from '../../shared/logger';
 import type {installFn, Plugin} from '../../shared/types';
 import {getWeather} from './api';
 
 async function weatherForecast(helper: Helper) {
-  const {data} = await getWeather();
-  if (data.code === '200') {
-    for (let i = 0; i < 2; i++) {
-      if (data.hourly[i].text.includes('雨')) {
-        helper.sendMsg('未来两小时内有雨，记得带伞');
-        break;
+  try {
+    const {data} = await getWeather();
+    if (data.code === '200') {
+      for (let i = 0; i < 2; i++) {
+        if (data.hourly[i].text.includes('雨')) {
+          helper.sendMsg('未来两小时内有雨，记得带伞');
+          break;
+        }
       }
     }
+  } catch (error) {
+    logger.error((error as Error).message);
   }
 }
 

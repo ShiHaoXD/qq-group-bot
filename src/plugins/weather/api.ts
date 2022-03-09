28808dcb2b65019ff5c2e34357aef12502dee8f0
Cansiny0320 <1136843910@qq.com>
2022-03-10 01:40:21 +0800

    feat: 增加每日天气

--- src/plugins/weather/api.ts
+++ src/plugins/weather/api.ts
@@ -6,7 +6,13 @@ const {get} = createAxiosInstance(
   {retry: true}
 );
 
-export interface GetWeatherRes {
+const KEY = 'b6604b9d2e8d41ba90a5c2c6ea35a3b7';
+
+const location = '106.36,29.32';
+
+const query = `key=${KEY}&location=${location}`;
+
+export interface GetWeatherHoursRes {
   code: string;
   updateTime: string;
   hourly: HourlyData[];
@@ -18,9 +24,23 @@ export interface HourlyData {
   text: string; // 天气状况
 }
 
+export interface GetWeatherDaysRes {
+  code: string;
+  updateTime: string;
+  daily: DailyData[];
+}
+
+export interface DailyData {
+  fxDate: string; // 预报日期
+  tempMax: string; // 最高气温
+  tempMin: string; // 最低气温
+  textDay: string; // 白天天气状况
+  textNight: string; // 晚上天气状况
+  precip: string; // 当天总降水量
+}
+
 // 完整接口文档：https://dev.qweather.com/docs/api/weather/weather-hourly-forecast/
 
-export const getWeather = () =>
-  get<GetWeatherRes>(
-    '/24h?key=b6604b9d2e8d41ba90a5c2c6ea35a3b7&location=106.36,29.32'
-  );
+export const getWeather24Hours = () => get<GetWeatherHoursRes>(`/24h?${query}`);
+
+export const getWeather3Days = () => get<GetWeatherDaysRes>(`/3d?${query}`);
--- src/plugins/weather/index.ts
+++ src/plugins/weather/index.ts
@@ -2,18 +2,70 @@ import {scheduleJob} from 'node-schedule';
 import type Helper from '../../Helper';
 import {logger} from '../../shared/logger';
 import type {installFn, Plugin} from '../../shared/types';
-import {getWeather} from './api';
+import {getWeather24Hours, getWeather3Days} from './api';
+
+let yesterdayAvgTem = NaN;
+
+async function willRainHorusLater(hour: number) {
+  const {data} = await getWeather24Hours();
+  if (data.code === '200') {
+    for (let i = hour; i < hour; i++) {
+      if (data.hourly[i].text.includes('雨')) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+async function dailyWeather(helper: Helper) {
+  try {
+    const {data} = await getWeather3Days();
+    if (data.code === '200') {
+      const {daily} = data;
+      const [todayWeather] = daily;
+      const result: string[] = [];
+      result.push('今日天气:');
+      result.push(`最高气温: ${todayWeather.tempMax}`);
+      result.push(`最低气温: ${todayWeather.tempMin}`);
+      result.push(
+        `天气: ${
+          todayWeather.textDay === todayWeather.textNight
+            ? todayWeather.textDay
+            : `${todayWeather.textDay}转${todayWeather.textNight}`
+        }`
+      );
+      todayWeather.precip !== '0.0' &&
+        result.push('今日可能下雨，出门建议带伞');
+      if (isNaN(yesterdayAvgTem)) {
+        yesterdayAvgTem =
+          (parseInt(todayWeather.tempMax, 10) +
+            parseInt(todayWeather.tempMin, 10)) /
+          2;
+        helper.sendMsg(result.join('\n'));
+        return;
+      }
+      const avgTem =
+        (parseInt(todayWeather.tempMax, 10) +
+          parseInt(todayWeather.tempMin, 10)) /
+        2;
+      result.push(
+        `今日平均气温较昨日${
+          avgTem > yesterdayAvgTem ? '升高' : '降低'
+        }${Math.abs(avgTem - yesterdayAvgTem)}度`
+      );
+      helper.sendMsg(result.join('\n'));
+      return;
+    }
+  } catch (error) {
+    logger.error((error as Error).message);
+  }
+}
 
 async function weatherForecast(helper: Helper) {
   try {
-    const {data} = await getWeather();
-    if (data.code === '200') {
-      for (let i = 0; i < 2; i++) {
-        if (data.hourly[i].text.includes('雨')) {
-          helper.sendMsg('未来两小时内有雨，记得带伞');
-          break;
-        }
-      }
+    if (await willRainHorusLater(2)) {
+      helper.sendMsg('未来两小时内有雨，记得带伞');
     }
   } catch (error) {
     logger.error((error as Error).message);
@@ -22,6 +74,7 @@ async function weatherForecast(helper: Helper) {
 
 const install: installFn = (_, helper) => {
   scheduleJob('0 */2 * * *', () => weatherForecast(helper));
+  scheduleJob('0 0 7 * * *', () => dailyWeather(helper));
 };
 
 export const WeatherForecast: Plugin = {
